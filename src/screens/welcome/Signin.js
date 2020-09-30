c69a4d3441d96660584cb51e34a1420cb8b59714
Umeraftab7747 <umeraftab7747@gmail.com>
2020-09-30 12:34:37 -0700

    modal

--- src/screens/welcome/Signin.js
+++ src/screens/welcome/Signin.js
@@ -1,44 +1,85 @@
 /* eslint-disable react/self-closing-comp */
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity, Modal} from 'react-native';
 import {
   widthPercentageToDP as w,
   heightPercentageToDP as h,
 } from 'react-native-responsive-screen';
 import {Black, red, white} from '../color';
 import {Appbtn, AppField} from '../../components';
+import {Icon} from 'react-native-elements';
+import {KeyboardAwareScrollView} from '@codler/react-native-keyboard-aware-scroll-view';
 
 export class Signin extends Component {
+  state = {
+    modalVisible: false,
+  };
   render() {
     return (
-      <View style={styles.MainContianer}>
-        <View style={styles.topContainer}>
-          <Text style={styles.header}>LOGIN</Text>
-        </View>
-        <View style={styles.middleContainer}>
-          <AppField placeholder={'Email'} name={'person-outline'} />
-          <AppField
-            placeholder={'Password'}
-            secureTextEntry={true}
-            name={'key-outline'}
-          />
-          <TouchableOpacity delayPressIn={0} style={styles.forgotpass}>
-            <Text style={styles.txtpass}>Forgot passowrd!</Text>
-          </TouchableOpacity>
-
-          <Appbtn title={'SignIn'} />
-        </View>
-        <View style={styles.bottomContainer}>
-          <View style={styles.newaccount}>
-            <View style={styles.notuser}>
-              <Text style={styles.usertxt}>Not a User!</Text>
-            </View>
-            <TouchableOpacity delayPressIn={0} style={styles.notuser2}>
-              <Text style={styles.signuptxt}>SignUp</Text>
-            </TouchableOpacity>
+      <KeyboardAwareScrollView>
+        <View style={styles.MainContianer}>
+          <View style={styles.topContainer}>
+            <Text style={styles.header}>LOGIN</Text>
           </View>
+          <View style={styles.middleContainer}>
+            <AppField placeholder={'Email'} name={'person-outline'} />
+            <AppField
+              placeholder={'Password'}
+              secureTextEntry={true}
+              name={'key-outline'}
+            />
+            <TouchableOpacity
+              onPress={() => {
+                this.setState({modalVisible: true});
+              }}
+              delayPressIn={0}
+              style={styles.forgotpass}>
+              <Text style={styles.txtpass}>Forgot passowrd!</Text>
+            </TouchableOpacity>
+
+            <Appbtn title={'SignIn'} />
+          </View>
+          <View style={styles.bottomContainer}>
+            <View style={styles.newaccount}>
+              <View style={styles.notuser}>
+                <Text style={styles.usertxt}>Not a User!</Text>
+              </View>
+              <TouchableOpacity delayPressIn={0} style={styles.notuser2}>
+                <Text style={styles.signuptxt}>SignUp</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          {/* modal */}
+          <Modal
+            animationType="fade"
+            transparent={true}
+            visible={this.state.modalVisible}
+            onRequestClose={() => {
+              this.setState({modalVisible: false});
+            }}>
+            <View style={styles.modalcontainer}>
+              <View style={styles.ModalViewcontainer}>
+                <View style={styles.leftContainer}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      this.setState({modalVisible: false});
+                    }}
+                    style={styles.iconmargin}>
+                    <Icon
+                      name={'close-circle-outline'}
+                      type="ionicon"
+                      color={white}
+                      size={30}
+                    />
+                  </TouchableOpacity>
+                </View>
+                <AppField placeholder={'Rest Email'} name={'mail-outline'} />
+                <Appbtn title={'Rest'} />
+              </View>
+            </View>
+          </Modal>
         </View>
-      </View>
+      </KeyboardAwareScrollView>
     );
   }
 }
@@ -112,5 +153,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     width: '39%',
+  },
+  modalcontainer: {
+    backgroundColor: '#fff9',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ModalViewcontainer: {
+    backgroundColor: '#e2e2e2',
+    width: '90%',
+    height: h('25%'),
+    borderRadius: h('2%'),
+    alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  leftContainer: {
+    // backgroundColor: white,
+    width: '100%',
+    height: h('7%'),
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingRight: h('1%'),
+  },
+  iconmargin: {
+    marginLeft: h('1%'),
   },
 });
