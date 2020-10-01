eb11c5747ccd34b4a08a989335cb80bf7891abd0
Umeraftab7747 <umeraftab7747@gmail.com>
2020-10-01 11:37:06 -0700

    urgent

--- src/screens/hire/Hiredashboard.js
+++ src/screens/hire/Hiredashboard.js
@@ -13,29 +13,114 @@ export class Hiredashboard extends Component {
   state = {
     modalVisible: false,
     data: [
-      {id: 1, name: 'umer', shield: true},
-      {id: 2, name: 'umer2', shield: false},
-      {id: 3, name: 'umer2', shield: false},
-      {id: 4, name: 'umer2', shield: true},
-      {id: 5, name: 'umer2', shield: true},
-      {id: 6, name: 'umer2', shield: true},
-      {id: 7, name: 'umer2', shield: true},
-      {id: 8, name: 'umer2', shield: true},
-      {id: 9, name: 'umer2', shield: true},
-      {id: 99, name: 'umer2', shield: true},
-      {id: 11, name: 'umer2', shield: true},
+      {
+        id: 1,
+        work: 'Plumber',
+        shield: true,
+        urgent: true,
+        date: '25.7.2020',
+        discription: 'i need a plumber with 2 year experince',
+      },
+      {
+        id: 2,
+        work: 'React',
+        shield: false,
+        urgent: false,
+        date: '25.7.2020',
+        discription: 'i need Reactjs developer with 1 year with good protflio',
+      },
+      {
+        id: 3,
+        work: 'React native',
+        shield: false,
+        urgent: false,
+        date: '25.7.2020',
+        discription: 'Reactnative with express js and 2 year experince ',
+      },
+      {
+        id: 4,
+        work: 'Firebase',
+        shield: true,
+        urgent: false,
+        date: '25.7.2020',
+        discription: 'firebase Developer with Android for Fulltime Job',
+      },
+      {
+        id: 5,
+        work: 'Wood Cuter',
+        shield: true,
+        urgent: true,
+        date: '25.7.2020',
+        discription: 'Wood Cuter require for 1 day job',
+      },
+      {
+        id: 6,
+        work: 'Carpanter',
+        shield: false,
+        urgent: false,
+        date: '25.7.2020',
+        discription: 'need a good Carpanter for a 1 day job',
+      },
+      {
+        id: 7,
+        work: 'Driver',
+        shield: false,
+        urgent: false,
+        date: '25.7.2020',
+        discription: 'Perment Driver Required for 1 year in verified Firm',
+      },
+      {
+        id: 8,
+        work: 'Php',
+        shield: true,
+        urgent: true,
+        date: '25.7.2020',
+        discription:
+          'Php developer with 8 year Experince required for Full time job',
+      },
+      {
+        id: 9,
+        work: 'Express.js',
+        shield: false,
+        urgent: false,
+        date: '25.7.2020',
+        discription: 'Express js developer required for 1 day work',
+      },
+      {
+        id: 99,
+        work: 'Backend',
+        shield: true,
+        urgent: true,
+        date: '25.7.2020',
+        discription: 'good backend Developer Required for part time work',
+      },
+      {
+        id: 11,
+        work: 'Ui/Ux',
+        shield: false,
+        urgent: false,
+        date: '25.7.2020',
+        discription:
+          'A person who have good Knowledge of Ui/ux can apply thanks',
+      },
     ],
     focused: true,
   };
 
   renderItem = (item, index) => (
     <View style={styles.flatlistCoaniner}>
-      <View style={styles.left}></View>
+      <View style={styles.left}>
+        <Text style={styles.work}>{item.work}</Text>
+        {item.urgent ? (
+          <View style={styles.urgnet}>
+            <Text style={styles.urgent}>URGENT</Text>
+          </View>
+        ) : null}
+      </View>
       <View style={styles.middle}>
-        <Text style={styles.work}>Plumber</Text>
-        <Text style={styles.discriptin}>
-          Work depend upon the user what he want to do
-        </Text>
+        {/* <Text style={styles.work}>Plumber</Text> */}
+        <Text style={styles.discriptin}>{item.discription}</Text>
+        <Text style={styles.date}>Date: {item.date}</Text>
       </View>
       <View style={styles.right}>
         <TouchableOpacity
@@ -142,7 +227,6 @@ const styles = StyleSheet.create({
     width: '35%',
     height: h('20%'),
     justifyContent: 'center',
-    alignItems: 'center',
   },
   middle: {
     // backgroundColor: 'yellow',
@@ -161,6 +245,7 @@ const styles = StyleSheet.create({
     fontSize: h('2.5%'),
     fontWeight: 'bold',
     color: Black,
+    marginLeft: h('1%'),
   },
   discriptin: {
     fontSize: h('2%'),
@@ -170,5 +255,26 @@ const styles = StyleSheet.create({
     color: red,
     fontSize: h('2%'),
     fontWeight: 'bold',
+  },
+  date: {
+    color: red,
+    fontWeight: 'bold',
+    fontSize: h('2%'),
+    marginTop: h('1%'),
+  },
+  urgnet: {
+    backgroundColor: 'rgba(235, 28, 45, 0.5)',
+    width: '70%',
+    height: '20%',
+    marginTop: h('3%'),
+    borderTopRightRadius: h('10%'),
+    borderBottomRightRadius: h('10%'),
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  urgent: {
+    color: white,
+    fontWeight: 'bold',
+    fontSize: h('2%'),
   },
 });
