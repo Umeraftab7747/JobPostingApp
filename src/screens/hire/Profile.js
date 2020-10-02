e59cbbe6734b6909b332e46cdcad80950871196f
Umeraftab7747 <umeraftab7747@gmail.com>
2020-10-01 17:34:06 -0700

    Profile

--- src/screens/hire/Profile.js
+++ src/screens/hire/Profile.js
@@ -1,6 +1,12 @@
 /* eslint-disable react/self-closing-comp */
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, TouchableOpacity, Modal} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+} from 'react-native';
 import {
   widthPercentageToDP as w,
   heightPercentageToDP as h,
@@ -8,13 +14,23 @@ import {
 import {Black, red, white} from '../color';
 import {Appbtn, AppField, Navheader, Searchbox} from '../../components';
 import {Icon} from 'react-native-elements';
-import {AnimatedFlatList, AnimationType} from 'flatlist-intro-animations';
 
 export class Profile extends Component {
   render() {
     return (
       <View style={styles.Container}>
-        <Text>HELLOW PROFILE HERE</Text>
+        <ImageBackground
+          source={require('../../assets/pbg.jpg')}
+          style={styles.top}>
+          <View style={styles.top2}>
+            <TouchableOpacity style={styles.topbtn}>
+              <Icon name={'create'} type="ionicon" color={white} size={35} />
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
+        <View style={styles.middle}>
+          <View style={styles.circle}></View>
+        </View>
       </View>
     );
   }
@@ -23,7 +39,35 @@ export class Profile extends Component {
 const styles = StyleSheet.create({
   Container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  top: {
+    // backgroundColor: 'rgba(235, 28, 45, 0.8)',
+    width: '100%',
+    height: h('30%'),
+  },
+  top2: {
+    backgroundColor: 'rgba(235, 28, 45, 0.8)',
+    width: '100%',
+    height: h('30%'),
+    justifyContent: 'flex-start',
+    alignItems: 'flex-end',
+  },
+  middle: {
+    // backgroundColor: 'gold',
+    width: '100%',
+    height: h('70'),
     alignItems: 'center',
+  },
+  topbtn: {
+    marginRight: h('2%'),
+    marginTop: h('1%'),
+  },
+  circle: {
+    height: 150,
+    width: 150,
+    borderRadius: 1000,
+    backgroundColor: white,
+    marginTop: -h('12%'),
+    elevation: 6,
   },
 });
